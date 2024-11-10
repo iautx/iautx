fbb9f9115789c5a5df415fd26a4ff34123e98e77
Roberto Andrade <roberto@randroid.com>
2024-11-10 08:35:14 -0300

    ✨ (package.json): add --remote flag to start script for remote development ✨ (index.js): add text translation and image generation features ♻️ (index.js): refactor image transcription to include translation 💡 (index.js): add TODO comments for future improvements

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 
-const CHAT_MODEL           = '@cf/meta/llama-3.1-8b-instruct-fast';
-const IMAGE_TO_TEXT_MODEL  = '@cf/llava-hf/llava-1.5-7b-hf'; //'@cf/meta/llama-3.2-11b-vision-instruct';
+const CHAT_MODEL           = '@cf/meta/llama-3.2-3b-instruct';
+const IMAGE_TO_TEXT_MODEL  = '@cf/meta/llama-3.2-11b-vision-instruct';
 const SPEECH_TO_TEXT_MODEL = '@cf/openai/whisper';
 const TEXT_TO_IMAGE_MODEL  = '@cf/black-forest-labs/flux-1-schnell';
+const TEXT_TO_TEXT_MODEL   = '@cf/meta/m2m100-1.2b';
+
+//TODO: Update webhook on GET
 
 async function transcribeAudio(env, audioUrl) {
   const audioResponse = await fetch(audioUrl);
@@ -18,27 +21,69 @@ async function transcribeAudio(env, audioUrl) {
   return modelResponse.text;
 }
 
+async function translateText(env, text) {
+  const modelResponse = await env.AI.run(TEXT_TO_TEXT_MODEL, {
+    text: text,
+    source_lang: 'en',
+    target_lang: 'pt'
+  });
+
+  console.log('modelResponse:', modelResponse);
+  return modelResponse.translated_text;
+}
+
 async function transcribeImage(env, imageUrl) {
   const imageResponse = await fetch(imageUrl);
   const blob = await imageResponse.arrayBuffer();
   
-  const modelResponse = await env.AI.run(IMAGE_TO_TEXT_MODEL, {
-    prompt: 'accept',
+  let modelResponse = await env.AI.run(IMAGE_TO_TEXT_MODEL, {
+    prompt: 'agree'
+  });
+
+  modelResponse = await env.AI.run(IMAGE_TO_TEXT_MODEL, {
+    prompt: 'descreva a imagem',
     image: [...new Uint8Array(blob)]
   });
 
   console.log('modelResponse:', modelResponse);
-  return modelResponse.text;
+  return await translateText(env, modelResponse.response);
+}
+
+
+async function generateImage(env, text) {
+  const modelResponse = await env.AI.run(TEXT_TO_IMAGE_MODEL, {
+    prompt: text
+  });
+
+  console.log('modelResponse:', modelResponse);
+  return modelResponse.image;
 }
 
 async function inferChat(env, text, phone, role = 'user') {
+  if (text.includes('imagem') || text.includes('foto') || text.includes('desenho')) {
+    const image = await generateImage(env, text);
+
+    return `data:image/png;base64,${image}`;
+  }
+
   const messages = [
     { 
       role: 'system', 
       content: 
-      `Você é um terapeuta especializado em acompanhar pacientes crianças e seus pais. 
-      Os dados do paciente estão estruturados em formato JSON a seguir: \`\`\`{}\`\`\`` 
+      `Você é um terapeuta especializado em acompanhar e dialogar com pacientes crianças e seus pais separadamente. 
+      Os dados do paciente estão estruturados em formato JSON a seguir: 
+
+      \`\`\`
+      {}
+      \`\`\`
+
+      O usuário pode ser o paciente ou o responsável, dependendo do número de telefone cadastrado.
+      Se não conseguir identificar o usuário, você pode perguntar diretamente.
+      Caso o usuário entre em contato, coletar relato e informações sobre o paciente para registrar no sistema.
+      `
     },
+    //TODO: JSON from KV
+    //TODO: Recent messages from KV
     { 
       role: role, 
       content: 
@@ -59,15 +104,26 @@ async function inferChat(env, text, phone, role = 'user') {
 }
 
 async function sendWhatsAppMessage(env, phone, text) {
-  console.log('sendWhatsAppMessage:', phone, text);
+  console.log('sendWhatsAppMessage:', phone, text.substring(0, Math.min(100, text.length)));
 
-  const zapiResponse = await fetch(`${env.Z_API_URL}/send-text`, { 
+  let endpoint = 'send-text';
+  let body = JSON.stringify({ 
+    phone: phone, 
+    message: text 
+  });
+
+  if (text.startsWith('data:image')) {
+    endpoint = 'send-image';
+    body = JSON.stringify({ 
+      phone: phone, 
+      image: text 
+    });
+  }
+
+  const zapiResponse = await fetch(`${env.Z_API_URL}/${endpoint}`, { 
     method: 'POST', 
     headers: { 'Content-Type': 'application/json', 'Client-Token': env.Z_API_TOKEN }, 
-    body: JSON.stringify({ 
-      phone: phone, 
-      message: text 
-    })
+    body: body
   });
 
   console.log('zapiResponse:', zapiResponse.statusText, await zapiResponse.text());
