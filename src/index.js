2b207ba1360c8a338b2da3ae52c6b81b385b2259
Roberto Andrade <roberto@randroid.com>
2024-11-10 04:53:22 -0300

    ✨ (index.js): implement audio processing and response handling for POST requests with status 'RECEIVED'

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
 export default {
   async fetch(request, env) {
-    /*
-    const audioResponse = await fetch(
-      'https://github.com/Azure-Samples/cognitive-services-speech-sdk/raw/master/samples/cpp/windows/console/samples/enrollment_audio_katie.wav'
-    );
-    const blob = await audioResponse.arrayBuffer();
-
-    const inputs = {
-      audio: [...new Uint8Array(blob)]
-    };
-    const response = await env.AI.run('@cf/openai/whisper', inputs);
-
-    return Response.json({ inputs, response });
-    */
    console.log('request:', request.method, request.url);
 
    if (request.method == 'POST') {
      const json = await request.json();
   
      console.log('json:', json);
+
+     if (json.status == 'RECEIVED') {
+        if (json.audio) {
+          const audioResponse = await fetch(json.audio.audioUrl);
+          const blob = await audioResponse.arrayBuffer();
+      
+          const inputs = {
+            audio: [...new Uint8Array(blob)]
+          };
+          const whisperResponse = await env.AI.run('@cf/openai/whisper', inputs);
+          console.log('whisperResponse:', whisperResponse);
+
+          const zapiResponse = await fetch(`${env.Z_API_URL}/send_text`, { phone: json.phone, text: whisperResponse.text });
+          console.log('zapiResponse:', zapiResponse);
+        }
+     }
    }
 
    return Response.json({ message: 'Received request' });
