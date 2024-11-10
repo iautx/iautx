5766c26062bbc01b36a848f9486ca3d482dc791b
Roberto Andrade <roberto@randroid.com>
2024-11-10 07:45:37 -0300

    ♻️ (index.js): refactor function signatures to include env parameter for better dependency management ✨ (index.js): update model constants to use new versions for improved performance 🐛 (index.js): fix incorrect variable usage in sendWhatsAppMessage function 💡 (index.js): add comments and logging for better debugging and understanding of code flow ✨ (index.js): add error handling to send error messages via WhatsApp on failure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 
-const CHAT_MODEL           = '@cf/meta/llama-3.2-3b-instruct';
-const IMAGE_TO_TEXT_MODEL  = '@cf/meta/llama-3.2-11b-vision-instruct';
+const CHAT_MODEL           = '@cf/meta/llama-3.1-8b-instruct-fast';
+const IMAGE_TO_TEXT_MODEL  = '@cf/llava-hf/llava-1.5-7b-hf'; //'@cf/meta/llama-3.2-11b-vision-instruct';
 const SPEECH_TO_TEXT_MODEL = '@cf/openai/whisper';
 const TEXT_TO_IMAGE_MODEL  = '@cf/black-forest-labs/flux-1-schnell';
 
-async function transcribeAudio(audioUrl) {
+async function transcribeAudio(env, audioUrl) {
   const audioResponse = await fetch(audioUrl);
   const blob = await audioResponse.arrayBuffer();
   
@@ -18,7 +18,7 @@ async function transcribeAudio(audioUrl) {
   return modelResponse.text;
 }
 
-async function transcribeImage(imageUrl) {
+async function transcribeImage(env, imageUrl) {
   const imageResponse = await fetch(imageUrl);
   const blob = await imageResponse.arrayBuffer();
   
@@ -31,30 +31,45 @@ async function transcribeImage(imageUrl) {
   return modelResponse.text;
 }
 
-async function inferChat(text, phone, role = 'user') {
-  const modelResponse = await env.AI.run(CHAT_MODEL, { 
+async function inferChat(env, text, phone, role = 'user') {
+  // const messages = [
+  //   { role: "system", content: "You are a friendly assistant" },
+  //   //{ role: 'system', content: 'Você é um terapeuta especializado em acompanhar pacientes crianças e seus pais. Os dados do paciente estão estruturados em formato JSON a seguir: ```{}```' },
+  //   { role: role, content: `Usuario com telefone ${phone} diz ${text}` }
+  // ];
+  // console.log('messages:', messages);
+
+  // const modelResponse = await env.AI.run(CHAT_MODEL, { 
+  //   messages
+  //   //functions: [],
+  //   //tools: []
+  // });
+
+  let chat = {
     messages: [
-      { role: 'system', content: 'Você é um terapeuta especializado em acompanhar pacientes crianças e seus pais. Os dados do paciente estão estruturados em formato JSON a seguir: ``````' },
-      { role: role, content: `Usuário com telefone ${phone}: ${text}` }
-    ],
-    functions: [],
-    tools: []
-  });
+      { role: 'system', content: 'You are a helpful assistant.' },
+      { role: 'user', content: 'Who won the world series in 2020?' }
+    ]
+  };
+  const modelResponse = await env.AI.run('@cf/meta/llama-3-8b-instruct', chat);
   
   console.log('modelResponse:', modelResponse);
 
   return modelResponse.response;
 }
 
-async function sendWhatsAppMessage(phoneNumber, text) {
+async function sendWhatsAppMessage(env, phone, text) {
+  console.log('sendWhatsAppMessage:', phone, text);
+
   const zapiResponse = await fetch(`${env.Z_API_URL}/send-text`, { 
     method: 'POST', 
     headers: { 'Content-Type': 'application/json', 'Client-Token': env.Z_API_TOKEN }, 
     body: JSON.stringify({ 
-      phone: json.phone, 
-      message: whisperResponse.text 
+      phone: phone, 
+      message: text 
     })
   });
+
   console.log('zapiResponse:', zapiResponse.statusText, await zapiResponse.text());
 
   return zapiResponse.status >= 200 && zapiResponse.status < 300;
@@ -64,32 +79,44 @@ export default {
   async fetch(request, env) {
    console.log('request:', request.method, request.url);
 
-   if (request.method == 'POST') {
-     const json = await request.json();
-  
-     console.log('json:', json);
+   let phone = null;
 
-     if (json.status == 'RECEIVED') {
-        if (json.audio) {
-          const text = await transcribeAudio(json.audio.audioUrl);
+   try {
+    if (request.method == 'POST') {
+      const json = await request.json();
+   
+      console.log('json:', json);
 
-          await sendWhatsAppMessage(json.phone, text);
-        }
+      phone = json.phone;
+ 
+      if (json.status == 'RECEIVED') {
+         if (json.audio) {
+           const text = await transcribeAudio(env, json.audio.audioUrl);
+ 
+           await sendWhatsAppMessage(env, phone, text);
+         }
+ 
+         if (json.image) {
+           const text = await transcribeImage(env, json.image.imageUrl);
+ 
+           await sendWhatsAppMessage(env, phone, text);
+         }
+ 
+         if (json.text) {
+           const text = await inferChat(env, json.text.message, phone);
+ 
+           await sendWhatsAppMessage(env, phone, text);
+         }
+      }
+    }
+ 
+    return Response.json({ message: 'Received request' });
+   } catch (error) {
+    console.error('error:', error);
 
-        if (json.image) {
-          const text = await transcribeImage(json.image.imageUrl);
+    await sendWhatsAppMessage(env, phone, `*Erro:* \`${error.message}\``);
 
-          await sendWhatsAppMessage(json.phone, text);
-        }
-
-        if (json.text) {
-          const text = await inferChat(json.text.message);
-
-          await sendWhatsAppMessage(json.phone, text);
-        }
-     }
+    return Response.json(error, { status: 500 });
    }
-
-   return Response.json({ message: 'Received request' });
   }
 };
